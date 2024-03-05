59c14fc8891be023415e150a08f0cfac49bb3ade
Shiva Sai Sunnam <shivasai.mudiraj12@gmail.com>
2024-03-05 14:08:50 +0530

    FIRST COMMIT

---
+++ JQuery.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    console.log("I'm using JQuery");
+    
+    //There are 3 types of Selector in JQuery
+
+    // 1. ELEMENT Selector
+    $('h3').click(function () {
+        console.log("Clicked on P", this);
+        // $(this).hide();
+    })
+    
+    // // 2. ID Selector
+    // $('#Even').click(function(){
+    //     console.log("You have Selected the Even row");
+    // })
+
+    // // 3. CLASS Selector
+    // $('.Odd').mouse(function(){
+    //     console.log("You have Selected the Odd row");
+    // })
+
+    // Other SELECTORS
+    $('p.odd').click();
+    $('h3:first').click();
+})
